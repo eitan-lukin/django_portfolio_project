67236576d5029a952c6c5c90a0effc9fdd616a7b
eitan <eitan.lukin@gmail.com>
2021-04-04 13:53:01 +0000

    removed cached files

--- personal_portfolio-project/personal_portfolio/settings.py
+++ personal_portfolio-project/personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'c28um@2rts6&7(ofkp%rcwbw&ctd=)-u&x+_*o%@-^@xho6m(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elukin.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
